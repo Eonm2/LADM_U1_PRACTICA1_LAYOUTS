9597765a083fd6652fac0189f4e877c51247f234
Erick Nolasco <erocnolascoma@ittepic.edu.mx>
2021-09-25 13:34:55 -0600

    Erick Octavio Nolasco Machuca - Paulina Alejandra Nova Ramirez - Practica1

---
+++ app/src/main/java/mx/tecnm/tepic/pantalla7.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalla7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla7)
+    }
+}
---
+++ app/src/main/java/mx/tecnm/tepic/pantalla8.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantalla8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla8)
+    }
+}
