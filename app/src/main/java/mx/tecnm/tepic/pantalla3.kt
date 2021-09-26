15b1825f66e0195c60fe59ee76f8b2f41171c445
Eonm2 <erocnolascoma@ittepic.edu.mx>
2021-09-25 20:38:21 -0600

    4 - Erick Octavio Nolasco Machuca - Paulina Alejandra Nova Ramirez - Practica1

--- app/src/main/java/mx/tecnm/tepic/pantalla3.kt
+++ app/src/main/java/mx/tecnm/tepic/pantalla3.kt
@@ -3,9 +3,9 @@ package mx.tecnm.tepic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity3 : AppCompatActivity() {
+class pantalla3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_pantalla3)
     }
 }
